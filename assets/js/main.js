7725115ae62aa1ed934333d7a8beafa3dfe7fef5
ToluFemiTayo <omodarafemi2123@gmail.com>
2018-11-19 18:06:22 +0100

    modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,26 @@
 
 
 
-  // Js on form
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+
 var _form = document.getElementsByClassName("form__fill")[0];
 var _inputs = Array.from(_form.getElementsByClassName("form-control"));
 var _error = 0;
@@ -61,4 +80,5 @@ _inputs.forEach(function (eachInputs) {
  _form.addEventListener("submit")
 e.preventDefault();
 // Js on form
+
 
