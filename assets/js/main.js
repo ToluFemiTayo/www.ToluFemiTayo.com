cddc24e850127207ebd23bde70420a53bd0046c7
ToluFemiTayo <omodarafemi2123@gmail.com>
2018-11-19 11:42:16 +0100

    responssiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,13 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+  function checkForm(form)
+  {
+    if(form.checkboxfield.checked) {
+      alert("The checkbox IS checked");
+    } else {
+      alert("The checkbox IS NOT checked");
+    }
+    return false;
+  }
